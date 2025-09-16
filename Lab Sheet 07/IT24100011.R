7e9d1782f5b12ffbfb43600bd6a2e075d4d3703d
IT24100011 <IT24100011@my.sliit.lk>
2025-09-16 12:44:18 +0530

    Lab sheet 7

---
+++ Lab Sheet 07/IT24100011.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\sdsjm\\OneDrive\\Desktop\\IT24100011")
+#(1)
+
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#(2)
+
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#(3) (i)
+
+1 - pnorm(130, mean=100, sd=15, lower.tail=TRUE)
+# OR
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+#(ii)
+
+qnorm(0.95, mean=100, sd=15, lower.tail=TRUE)
